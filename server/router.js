ceeb26301574e02fa469e98b6d2feb3a3f218808
waliang.wang <waliang.wang@qunar.com>
2017-12-08 15:31:10 +0800

    fix: 添加同构

--- server/router.js
+++ server/router.js
@@ -7,6 +7,12 @@ const React = config.React
 const { renderToString } = config.renderToString
 const Content = require('./template.jsx')
 
+const { match, RouterContext } = require('react-router')
+const routers = require('../client/src/Routes.js')
+
+// console.log(routers)
+
+
 router.get('/index', async (ctx, next) => {
   ctx.response.type = 'plain'
   ctx.body = renderToString(Content)
